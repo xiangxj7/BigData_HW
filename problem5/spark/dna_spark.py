6c295c04d29734ed3ca9ba842d1cfdd5e992d951
xiangxj7 <3106005662@qq.com>
2019-11-19 22:37:06 +0800

    spark --->  problem 5 finish test

---
+++ problem5/spark/dna_spark.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from pyspark import SparkContext
+import json
+
+
+
+sc = SparkContext('local', 'test')
+textFile = sc.textFile("file:///home/xiangxj7/BigData/MapReduce Assignments(data)/problem5//dna.json")
+dnaRRD = textFile.map(lambda row: json.loads(row))
+dnaRRD = dnaRRD.map(lambda record: (record[1][:-10], record[0]))
+dnaRRD = dnaRRD.reduceByKey(lambda a, b: a).map(lambda x: x[0])
+dnaRRD.foreach(print)
+dnaRRD.saveAsTextFile("spark_result.txt")
