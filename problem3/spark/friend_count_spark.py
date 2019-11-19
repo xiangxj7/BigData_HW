3f4207cd4fbd4d4c5ec94c78430458eb8eeb763a
xiangxj7 <3106005662@qq.com>
2019-11-19 21:46:19 +0800

    spark --->  problem 3 finish test

---
+++ problem3/spark/friend_count_spark.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+from pyspark import SparkContext
+import json
+
+sc = SparkContext('local', 'test')
+textFile = sc.textFile("file:///home/xiangxj7/BigData/MapReduce Assignments(data)/problem3//friends.json")
+friendCount = textFile.map(lambda row: json.loads(row))
+friendCount = friendCount.map(lambda record: (record[0], 1))
+friendCount = friendCount.reduceByKey(lambda a, b: a + b)
+
+friendCount.foreach(print)
+friendCount.saveAsTextFile("spark_result.txt")
